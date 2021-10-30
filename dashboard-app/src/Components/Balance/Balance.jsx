cb9e06df89f1c568bccb72e4185fcc838e3643f4
Bilal Zia <93026115+MohiBilal@users.noreply.github.com>
2021-10-30 06:32:06 -0700

    added balance component

---
+++ dashboard-app/src/Components/Balance/Balance.jsx
@@ -0,0 +1,24 @@
+import {react}from'react'
+import './Balance.css';
+
+
+const Balance=()=>{
+
+    return(
+        <div>
+            <div class="section">
+            
+            <p class="title">YOUR BALANCE</p>
+            <p class="title2">TOTAL EARNED</p>
+            <img class="icon" src="crypt.png" alt="crypt token"></img>
+            <img class="icon2" src="busd.png" alt="busd"></img>
+            <p class="ammount">777,777,777,778</p>
+            <p class="ammount2">$452.21</p>
+         
+     </div>
+
+        </div>
+    );
+
+}
+export default Balance        
