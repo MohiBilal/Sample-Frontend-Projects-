1c9da1c2304ed392131c926bfa7b4034a01f507c
Bilal Zia <93026115+MohiBilal@users.noreply.github.com>
2021-12-26 20:12:36 -0800

    added title, amount and itemimg prop

--- agri-app/src/App.js
+++ agri-app/src/App.js
@@ -1,15 +1,13 @@
 import logo from './logo.svg';
 import Header from './Components/Header/Header';
 import Item from './Components/Item/Item';
-
-
-
+import Headphones from './Components/Item/Headphones.jpg'
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <Item icon = {logo} />
+      <Item itemimg={Headphones} amount="1000" title="Headphones" />
     </div>
   );
 }
--- agri-app/src/Components/Item/Item.jsx
+++ agri-app/src/Components/Item/Item.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import './Item.css'
-import logo from './headphones.jpg'
+import Headphones from './Headphones.jpg'
 
 export default function Item(props) {
     console.log("props",props)
     return (
         <div>
              <div class="items">
-        <img class="imgsizechange" src={props.logo}></img>
+        <img class="imgsizechange" src={props.itemimg}></img>
         <div class="item-details">
-            <p class="title">Lorem ipsum dolor, sit amet conse<br></br>ctetur adipisicing elit.</p>
-            <p class="ammount">2799.00</p>
+            <p class="title">{props.title}</p>
+            <p class="ammount">{props.amount}</p>
             <p class="currency">AED</p>
             <p class="canceled-text">3200.00</p>
             <p class="canceled-text">AED</p>
